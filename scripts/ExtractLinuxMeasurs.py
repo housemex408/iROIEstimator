859270f5c1eea4a1287a65735a3313fb4fa6e7e8
Jose Alvarado <jalvarado80@gmail.com>
2020-03-15 23:32:03 -0700

    adding T_Line and T_Module

--- scripts/ExtractLinuxMeasurs.py
+++ scripts/ExtractLinuxMeasurs.py
@@ -1,8 +1,8 @@
 import subprocess
 
 tags = open('tags.txt', 'r')
-data_analysis = open("./output/data_analysis.csv", "w")
-header = 'Version, NC, NO, E_Module, E_Line'
+data_analysis = open("./output/data_analysis_part2.csv", "w")
+header = 'Version, NC, NO, E_Module, E_Line, T_Module, T_Line'
 data_analysis.write(header)
 fromVersion = tags.readline()
 toVersion = tags.readline()
@@ -18,10 +18,10 @@ while True:
     changeLog = '{filename}_changelog.txt'.format(filename=toVersion)
 
     # Get the change log for the current versionRange
-    getChangeLog = 'git log --pretty="%h - %s (%an)" {range} > ../scripts/output/{outputFile}'.format(range=versionRange, outputFile=changeLog)
+    #getChangeLog = 'git log --pretty="%h - %s (%an)" {range} > ../scripts/output/{outputFile}'.format(range=versionRange, outputFile=changeLog)
 
     #print(getChangeLog)
-    process = subprocess.run([getChangeLog], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
+    #process = subprocess.run([getChangeLog], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
 
  
     # Get corrective tasks
@@ -64,14 +64,34 @@ while True:
     msg = 'E_Line: {E_Line}'.format(E_Line=E_Line)
     print(msg)
 
+    # Checkout tags/toVersion
+    checkoutTag = "git checkout tags/" + toVersion + " --force"
+    print(checkoutTag)
+    process = subprocess.run([checkoutTag], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
+    print(process.stdout.strip())
+
     # Get total files up to this version
+    getTotalFiles = "git ls-files --exclude-standard | wc -l | awk '{print $1 }'"
+    process = subprocess.run([getTotalFiles], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
+    T_Module = process.stdout.strip()
+    msg = 'T_Module: {T_Module}'.format(T_Module=T_Module)
+    print(msg)
 
     # Get total LOC up to this version
+    getTotalLOC = "git grep ^ | wc -l | awk '{print $1 }'"
+    process = subprocess.run([getTotalLOC], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
+    T_Line = process.stdout.strip()
+    msg = 'T_Line: {T_Line}'.format(T_Line=T_Line)
+    print(msg)
+
+    # Checkout master
+    checkoutMaster = "git checkout master --force"
+    process = subprocess.run([checkoutMaster], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd='../linux/')
+    print(process.stdout.strip())
 
     # Create a line item { toVersion, NC, NO, E_Module, E_Line, T_Module, T_Line }
-    
-    template = '\n{toVersion}, {NC}, {NO}, {E_Module}, {E_Line}'
-    line = template.format(toVersion=toVersion, NC=NC, NO=NO, E_Module=E_Module, E_Line=E_Line)
+    template = '\n{toVersion}, {NC}, {NO}, {E_Module}, {E_Line}, {T_Module}, {T_Line}'
+    line = template.format(toVersion=toVersion, NC=NC, NO=NO, E_Module=E_Module, E_Line=E_Line, T_Module=T_Module, T_Line=T_Line)
     data_analysis.write(line)
 
     fromVersion = toVersion
