799056f1be019386aeddd895662a00f6bf25f463
Jose Alvarado <jalvarado80@gmail.com>
2020-05-03 16:53:24 -0700

    forecasting in progress

--- scripts/iROIEstimator.py
+++ scripts/iROIEstimator.py
@@ -7,13 +7,15 @@ from sklearn import metrics
 from tabulate import tabulate
 from sklearn.ensemble import RandomForestRegressor
 from scipy.stats import shapiro
+import matplotlib.pyplot as plt
 from scipy.special import inv_boxcox
 from scipy.stats import boxcox
+from fbprophet import Prophet
 sys.path.append(os.path.abspath(__file__))
 import Utilities as utils
 import Constants as c
 
-class EffortMeasurements:
+class Effort:
     def __init__(self, project, value, task, df):
         self.type = value
         self.task = task
@@ -39,10 +41,10 @@ class EffortMeasurements:
         self.Y = None
         self.results = None
 
-    def forecast_effort(self):
+    def forecast_variable(self, variable, df, predicton_months):
         data = {
             c.DATE: self.df.index,
-            c.NT: self.df[self.type]
+            c.NT: self.df[variable]
         }
         NT = pd.DataFrame(data) 
 
@@ -52,7 +54,7 @@ class EffortMeasurements:
 
         m_NT = Prophet(interval_width=0.90)
         m_NT.fit(NT)
-        future_NT = m_NT.make_future_dataframe(periods=months, freq='m')
+        future_NT = m_NT.make_future_dataframe(periods=predicton_months, freq='m')
         forecast_NT = m_NT.predict(future_NT)
 
         m_NT.plot(forecast_NT)
@@ -72,6 +74,15 @@ class EffortMeasurements:
         self.forecast = forecast_NT_inv
 
         return self.forecast
+
+    def forecast_effort(self, data, dateIndex, variable, rf_regressor):
+        X_Future = pd.DataFrame(data) 
+        y_pred_rf = rf_regressor.predict(X_Future)
+        y_pred_index = dateIndex
+
+        resultData = {variable: y_pred_rf.round(2), c.DATE: y_pred_index}
+        results = pd.DataFrame(resultData) 
+        return results
     
     def predict_effort(self):
         if self.type == c.LINE_CC:
@@ -131,21 +142,26 @@ class EffortMeasurements:
 class iROIEstimator:
     cwd = "scripts/exports"
 
-    def __init__(self, project):
+    def __init__(self, project, prediction_years=3):
         self.project_name = project.split('/')[1]
         self.file_template = "{cwd}/{project_name}/{project_name}_dataset_{task}.csv"    
-        self.line_cc = None;
-        self.line_ec = None;
-        self.module_cc = None;
-        self.module_ec = None;
+        self.line_cc = None
+        self.line_ec = None
+        self.module_cc = None
+        self.module_ec = None
+        self.prediction_years = prediction_years
+        self.predicton_months = self.prediction_years / 12
     
     def execute(self):
         for task in c.TASK_LIST:
             tasks = self.file_template.format(cwd=self.cwd, project_name=self.project_name, task = task)
             df = pd.read_csv(tasks)
             
+            df = df.dropna(subset=[c.T_MODULE])
+            df = df.dropna(subset=[c.DATE])
             df[c.DATE] = pd.to_datetime(df[c.DATE])
-            df = df.dropna(subset=[c.TASK])
+            df = df.set_index(c.DATE)
+
             df.fillna(df.mean(), inplace=True)
             if df.isna().values.any():
                 df.fillna(0, inplace=True)
@@ -157,10 +173,11 @@ class iROIEstimator:
                 break
             
             self.predict_effort(task, df)
+            # self.forecast_effort(df)
     
     def predict_effort(self, task, df):
         # LINE_CC
-        self.line_cc = EffortMeasurements(self.project_name, c.LINE_CC, task, df)
+        self.line_cc = Effort(self.project_name, c.LINE_CC, task, df)
         line_cc_results = self.line_cc.predict_effort()
         print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, c.LINE_CC, line_cc_results.size))
         self.line_cc.calculate_perf_measurements()
@@ -168,7 +185,7 @@ class iROIEstimator:
         print(line_cc_output.head())
 
         # LINE_EC
-        self.line_ec = EffortMeasurements(self.project_name, c.LINE_EC, task, df)
+        self.line_ec = Effort(self.project_name, c.LINE_EC, task, df)
         line_ec_results = self.line_ec.predict_effort()
         print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, c.LINE_EC, line_ec_results.size))
         self.line_ec.calculate_perf_measurements()
@@ -176,37 +193,49 @@ class iROIEstimator:
         print(line_ec_output.head())
 
         # MODULE_CC
-        self.module_cc = EffortMeasurements(self.project_name, c.MODULE_CC, task, df)
+        self.module_cc = Effort(self.project_name, c.MODULE_CC, task, df)
         module_cc_results = self.module_cc.predict_effort()
         print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, c.MODULE_CC, module_cc_results.size))
         self.module_cc.calculate_perf_measurements()
         module_cc_output = self.module_cc.create_output_df()
         print(module_cc_output.head())
-        
 
         # MODULE_EC
-        self.module_ec = EffortMeasurements(self.project_name, c.MODULE_EC, task, df)
+        self.module_ec = Effort(self.project_name, c.MODULE_EC, task, df)
         module_ec_results = self.module_ec.predict_effort()
         print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, c.MODULE_EC, module_ec_results.size))
         self.module_ec.calculate_perf_measurements()
         module_ec_output = self.module_ec.create_output_df()
         print(module_ec_output.head())
 
-    # def forecastEffort(self):
+    def forecast_effort(self, df):
         # FORECASTING
-        # for task in c.TASK_LIST:
-        #     forecast_NT = forecastVariable(c.NT_CC, df, predictionMonths)
-        #     forecast_NO = forecastVariable(c.NO_CC, df, predictionMonths)
-        #     forecast_T_Module= forecastVariable(c.T_MODULE, df, predictionMonths)
+        forecast_NT = self.module_cc.forecast_variable(c.NT_CC, df, self.predicton_months)
+        forecast_NO = self.module_cc.forecast_variable(c.NO_CC, df, self.predicton_months)
+        forecast_T_Module= self.module_cc.forecast_variable(c.T_MODULE, df, self.predicton_months)
 
-        #     data = {
-        #         c.NT_CC: forecast_NT['yhat'],
-        #         c.NO_CC: forecast_NO['yhat'],
-        #         c.T_MODULE: forecast_T_Module['yhat']
-        #     }
-        #     dateIndex = forecast_NT['ds']
-        #     results = predictFutureEffort(data, dateIndex, c.MODULE_CC, rf_regressor)
-        #     displayFutureEffort(results, c.MODULE_CC, predictionYears)
+        data = {
+            c.NT_CC: forecast_NT['yhat'],
+            c.NO_CC: forecast_NO['yhat'],
+            c.T_MODULE: forecast_T_Module['yhat']
+        }
+        dateIndex = forecast_NT['ds']
+        results = self.line_cc.forecast_effort(data, dateIndex, c.MODULE_CC, self.line_cc.model)
+        self.displayFutureEffort(results, c.MODULE_CC, self.prediction_years)
+
+    def displayFutureEffort(self, results, variable, predictionYears):
+        startDate = results.tail(predictionYears * 12).iloc[0][c.DATE]
+        results['Year'] = results[c.DATE].apply(lambda x: x.year)
+        results = pd.pivot_table(results,index=["Year"],values=[variable], aggfunc=np.sum).tail(predictionYears + 1)
+
+        objects = results.index
+        y_pos = np.arange(len(objects))
+        performance = results[variable]
+
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.xticks(y_pos, objects)
+        plt.ylabel(variable)
+        plt.title('Effort {0} Years From {1}'.format(predictionYears, startDate.strftime('%Y-%m-%d')))
     
 
     # def calculateROI():
