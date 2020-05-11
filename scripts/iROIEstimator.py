c84d6da140531147a9d3470bd8ec1d092812d4e5
Jose Alvarado <jalvarado80@gmail.com>
2020-05-10 21:21:32 -0700

    added logging

--- scripts/iROIEstimator.py
+++ scripts/iROIEstimator.py
@@ -7,18 +7,17 @@ sys.path.append(os.path.abspath(__file__))
 import Utilities as utils
 import Constants as c
 from Effort import Effort
-
+logger = utils.get_logger()
 
 class iROIEstimator:
     # input = "../../exports"
     input = "scripts/exports"
     output = "scripts/notebook/results"
     TASK_LIST = c.TASK_LIST
-    # TASK_LIST = ["BUG"]
+    # TASK_LIST = ["DOCS"]
     results_header = [c.DATE, c.PROJECT, c.MODEL, c.TASK, c.NT, c.NO, c.T_MODULE, c.OBSERVED, c.PREDICTED, c.DIFFERENCE, c.PERCENT_ERROR]
     performance_measures_header = [c.PROJECT, c.MODEL, c.TASK, c.R_SQUARED, c.R_SQUARED_ADJ, c.MAE, c.MSE, c.RMSE, c.PRED_25, c.PRED_50, c.T_RECORDS]
     roi_header = [c.PROJECT, c.MODEL, c.AMOUNT_INVESTED, c.AMOUNT_RETURNED, c.INVESTMENT_GAIN, c.ROI, c.ANNUALIZED_ROI]
-    
 
     def __init__(self, project, model=c.LINE, prediction_years=3):
         self.project_name = project.split('/')[1]
@@ -30,7 +29,7 @@ class iROIEstimator:
         self.module_ec = None
         self.prediction_years = prediction_years
         self.predicton_months = self.prediction_years * 12
-        self.task_forecasted_effort = {};
+        self.task_forecasted_effort = {}
         self.amount_invested = 0
         self.amount_returned = 0
         self.investment_gain = 0
@@ -91,22 +90,22 @@ class iROIEstimator:
         if self.model == c.MODULE:
             CC = c.MODULE_CC
             EC = c.MODULE_EC
-        
+
         return CC, EC
 
     def predict_effort(self, task, df):
-        
+
         CC, EC = self.get_independent_variables()
 
         self.module_cc = Effort(self.project_name, self.model, CC, task, df)
         module_cc_results = self.module_cc.predict_effort()
-        print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, CC, module_cc_results.size))
+        logger.info("{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, CC, module_cc_results.size))
         self.module_cc.calculate_perf_measurements()
         module_cc_output = self.module_cc.create_output_df()
 
         self.module_ec = Effort(self.project_name, self.model, EC, task, df)
         module_ec_results = self.module_ec.predict_effort()
-        print("\n{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, EC, module_ec_results.size))
+        logger.info("{0} - {1} - {2} prediction count: {3}".format(self.project_name, task, EC, module_ec_results.size))
         self.module_ec.calculate_perf_measurements()
         module_ec_output = self.module_ec.create_output_df()
 
@@ -126,15 +125,15 @@ class iROIEstimator:
 
     def display_forecast(self, predictionYears):
         for key in self.task_forecasted_effort:
-            print("\n{0} - {1} Forecasted Effort: \n".format(self.project_name, key))
-            print(self.task_forecasted_effort[key][c.MODULE_CC].T)
-            print(self.task_forecasted_effort[key][c.MODULE_EC].T)
+            logger.info("{0} - {1} Forecasted Effort: \n".format(self.project_name, key))
+            logger.info(self.task_forecasted_effort[key][c.MODULE_CC].T)
+            logger.info(self.task_forecasted_effort[key][c.MODULE_EC].T)
 
             self.results[c.DATE] = self.results.index
             self.results.sort_values(by=[c.DATE, c.PROJECT, c.MODEL, c.TASK], ascending = True, inplace=True)
             self.performance_measures.sort_values(by=[c.PROJECT, c.MODEL, c.TASK], inplace=True)
-            print(self.results)
-            print(self.performance_measures)
+            logger.info("\n {0}".format(self.results))
+            logger.info("\n {0}".format(self.performance_measures))
 
     def save_results_performance_measures(self):
         self.results.to_csv(self.results_file, header=False, mode = 'a', index=False)
@@ -157,7 +156,7 @@ class iROIEstimator:
         effort_ec = 0.0
         self.amount_invested = 0
         self.amount_returned = 0
-        self.investment_gain = 0;
+        self.investment_gain = 0
         self.roi = 0
         self.annualized_roi = 0
 
@@ -166,19 +165,19 @@ class iROIEstimator:
             effort_ec = self.task_forecasted_effort[key][c.MODULE_EC].values.sum()
             self.amount_invested = self.amount_invested + effort_cc
             self.amount_returned = self.amount_returned + effort_ec
-            print("{0} - {1} CC Forecasted Effort: {2}".format(self.project_name, key, round(effort_cc), 2))
-            print("{0} - {1} EC Forecasted Effort: {2}".format(self.project_name, key, round(effort_ec), 2))
+            logger.info("{0} - {1} CC Forecasted Effort: {2}".format(self.project_name, key, round(effort_cc), 2))
+            logger.info("{0} - {1} EC Forecasted Effort: {2}".format(self.project_name, key, round(effort_ec), 2))
 
-        print("{0} - Core Contributor Forecasted Effort Over {1} years: {2}".format(self.project_name, self.prediction_years, round(self.amount_invested, 2)))
-        print("{0} - External Contributor Forecasted Effort Over {1} years: {2}".format(self.project_name, self.prediction_years, round(self.amount_returned, 2)))
+        logger.info("{0} - Core Contributor Forecasted Effort Over {1} years: {2}".format(self.project_name, self.prediction_years, round(self.amount_invested, 2)))
+        logger.info("{0} - External Contributor Forecasted Effort Over {1} years: {2}".format(self.project_name, self.prediction_years, round(self.amount_returned, 2)))
 
         self.calculate_investment_gain()
         self.calculate_ROI()
         self.calculate_annualized_ROI()
 
-        print("{0} - Investment Gain: {1}".format(self.project_name, self.investment_gain))
-        print("{0} - ROI: {1}".format(self.project_name, self.roi))
-        print("{0} - Annualized ROI: {1}".format(self.project_name, self.annualized_roi))
+        logger.info("{0} - Investment Gain: {1}".format(self.project_name, self.investment_gain))
+        logger.info("{0} - ROI: {1}".format(self.project_name, self.roi))
+        logger.info("{0} - Annualized ROI: {1}".format(self.project_name, self.annualized_roi))
 
     def save_results_roi_measures(self):
         roi_measures = pd.DataFrame({c.PROJECT: [self.project_name],
@@ -192,10 +191,15 @@ class iROIEstimator:
         self.roi_measures = pd.concat([self.roi_measures, roi_measures])
         self.roi_measures.to_csv(self.roi_measures_file, header=False, mode = 'a', index=False)
 
-
-project_list = ["vuejs/angular.js"]
-# project_list = c.PROJECT_LIST
+# project_list = ["vuejs/lock"]
+project_list = c.PROJECT_LIST
 
 for p in project_list:
-  estimator = iROIEstimator(p, c.LINE)
-  estimator.execute()
+  try:
+    logger.debug("Project {0}".format(p))
+    estimator = iROIEstimator(p, c.MODULE)
+    estimator.execute()
+  except Exception:
+    logger.error("Error:  {0}".format(p), exc_info=True)
+    continue
+
