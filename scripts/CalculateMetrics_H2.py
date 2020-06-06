fixed pred_25 calculation

scripts/CalculateMetrics_H2.py
@@ -83,19 +83,7 @@ def createDF(project_name, model, task, r_squared, r_squared_adj, mae, mse, rmse
                       c.P_NA: [p_na]})
   return row_df
 
-
-def compareResults(Y, predictions):
-  data = {}
-  data[c.OBSERVED] = Y.round(2)
-  data[c.PREDICTED] = predictions.round(2)
-  data[c.DIFFERENCE] = abs(Y - predictions).round(2)
-  data[c.PERCENT_ERROR] = (abs(Y - predictions)/Y).round(2)
-  results = pd.DataFrame(data)
-  results[c.PERCENT_ERROR].fillna(0, inplace=True)
-  results[c.PERCENT_ERROR].replace(np.inf, 0, inplace=True)
-  return results
-
-def calculate_effort(X, Y, project, task, model_type, transformer, regressor):
+def calculate_effort(X, Y, project, task, model_type, transformer, regressor, i_records, t_records):
 
   dummy_df = X.copy()
   dummy_df["Y"] = Y
@@ -119,7 +107,8 @@ def calculate_effort(X, Y, project, task, model_type, transformer, regressor):
 
   kfold = model_selection.KFold(n_splits=splits)
   predictions = cross_val_predict(model, X, Y, cv=kfold)
-  results = compareResults(Y, predictions)
+  # results = compareResults(Y, predictions)
+  results = utils.create_percent_error_df(Y, predictions)
 
   r_squared, r_squared_adj, mae, mse, rmse, pred25, pred50 = extractPerfMeasures(model, Y, predictions, results, X)
   row = createDF(project, model_type, task, r_squared, r_squared_adj, mae, mse, rmse, pred25, pred50, t_records, i_records - t_records, p_na)
@@ -130,7 +119,7 @@ def calculate_effort(X, Y, project, task, model_type, transformer, regressor):
 
 # BEGIN Main
 directoryPath = "scripts/exports"
-outputFile = "scripts/notebook/results/h2_metrics_h1_DT_05_29_2020.csv".format(directory=directoryPath)
+outputFile = "scripts/notebook/results/h2_metrics_h1_DT_06_06_2020.csv".format(directory=directoryPath)
 headers = [c.PROJECT, c.MODEL, c.TASK, c.R_SQUARED, c.R_SQUARED_ADJ, c.MAE, c.MSE, c.RMSE, c.PRED_25, c.PRED_50, c.T_RECORDS, c.D_RECORDS, c.P_NA]
 o_df = pd.DataFrame(columns=headers)
 
@@ -156,16 +145,7 @@ for task in c.TASK_LIST:
   i_records = len(df)
 
   # df = utils.isRegularVersion(df)
-
-  # df[c.NT] = df[[c.NT_CC, c.NT_EC, c.NT_UC]].sum(axis=1)
-  # df[c.NO] = df[[c.NO_CC, c.NO_EC, c.NO_UC]].sum(axis=1)
-  # df[c.LINE] = df[[c.LINE_CC, c.LINE_EC, c.LINE_UC]].sum(axis=1)
-  # df[c.MODULE] = df[[c.MODULE_CC, c.MODULE_EC, c.MODULE_UC]].sum(axis=1)
-  # df[c.T_CONTRIBUTORS] = df[[c.T_CC, c.T_EC, c.T_UC]].sum(axis=1)
-  # df["T_LINE_DIFF"] = df[c.T_LINE].diff(-1)
-  # df["T_MODULE_DIFF"] = df[c.T_MODULE].diff(-1)
-  # df.dropna(subset=[c.MODULE, c.LINE, c.NT, c.NO, c.T_CONTRIBUTORS], inplace=True)
-  df[c.T_LINE + "_P"] = df[c.T_LINE].shift()
+  df[c.T_LINE_P] = df[c.T_LINE].shift()
 
   # df[c.T_CONTRIBUTORS] = df[c.T_CONTRIBUTORS] + 2
 
@@ -176,20 +156,14 @@ for task in c.TASK_LIST:
       continue
 
   # Calculate LINE
-  # line_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC]], df[c.LINE_CC], project, task, c.LINE_CC, transformer, regressor)
-  # line_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC]], df[c.LINE_EC], project, task, c.LINE_EC, transformer, regressor)
-  # line_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC]], df[c.LINE_UC], project, task, c.LINE_UC, transformer, regressor)
-  line_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC, c.T_LINE + "_P"]], df[c.LINE_CC], project, task, c.LINE_CC, transformer, regressor)
-  line_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC, c.T_LINE + "_P"]], df[c.LINE_EC], project, task, c.LINE_EC, transformer, regressor)
-  line_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC, c.T_LINE + "_P"]], df[c.LINE_UC], project, task, c.LINE_UC, transformer, regressor)
+  line_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC, c.T_LINE_P]], df[c.LINE_CC], project, task, c.LINE_CC, transformer, regressor, i_records, t_records)
+  line_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC, c.T_LINE_P]], df[c.LINE_EC], project, task, c.LINE_EC, transformer, regressor, i_records, t_records)
+  line_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC, c.T_LINE_P]], df[c.LINE_UC], project, task, c.LINE_UC, transformer, regressor, i_records, t_records)
 
   # Calculate MODULE
-  # module_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC]], df[c.MODULE_CC], project, task, c.MODULE_CC, transformer, regressor)
-  # module_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC]], df[c.MODULE_EC], project, task, c.MODULE_EC, transformer, regressor)
-  # module_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC]], df[c.MODULE_UC], project, task, c.MODULE_UC, transformer, regressor)
-  module_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC, c.T_LINE + "_P"]], df[c.MODULE_CC], project, task, c.MODULE_CC, transformer, regressor)
-  module_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC, c.T_LINE + "_P"]], df[c.MODULE_EC], project, task, c.MODULE_EC, transformer, regressor)
-  module_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC, c.T_LINE + "_P"]], df[c.MODULE_UC], project, task, c.MODULE_UC, transformer, regressor)
+  module_cc_output = calculate_effort(df[[c.NT_CC, c.NO_CC, c.T_CC, c.T_LINE_P]], df[c.MODULE_CC], project, task, c.MODULE_CC, transformer, regressor, i_records, t_records)
+  module_ec_output = calculate_effort(df[[c.NT_EC, c.NO_EC, c.T_EC, c.T_LINE_P]], df[c.MODULE_EC], project, task, c.MODULE_EC, transformer, regressor, i_records, t_records)
+  module_uc_output = calculate_effort(df[[c.NT_UC, c.NO_UC, c.T_UC, c.T_LINE_P]], df[c.MODULE_UC], project, task, c.MODULE_UC, transformer, regressor, i_records, t_records)
 
   output = pd.concat([line_cc_output, line_ec_output, line_uc_output, module_cc_output, module_ec_output, module_uc_output])
 