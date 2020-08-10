40f3bb388168afee9ab11052101eb921abffb2c4
Jose Alvarado <jalvarado80@gmail.com>
2020-08-09 23:30:31 -0700

    adding spearman and pearson correlations

--- scripts/ExploreCorrelations.py
+++ scripts/ExploreCorrelations.py
@@ -14,17 +14,18 @@ os.environ["NUMEXPR_MAX_THREADS"] = "12"
 
 # BEGIN Main
 directoryPath = "scripts/exports"
-outputFile = "scripts/notebook/results/correlations/{project}_correlations.csv"
+outputFile = "scripts/notebook/data_analysis/correlations/{project}_correlations.csv"
 
-# for project in c.ALL_PROJECTS:
-for project in ["angular"]:
+for project in c.ALL_PROJECTS:
+# for project in ["angular"]:
   # for task in ["BUG"]:
+
+  project = project.split('/')[1]
+  correlationsFile = open(outputFile.format(project=project), "w")
+
   for task in c.TASK_LIST:
 
     tasks = "{directoryPath}/{project_name}/{project_name}_dataset_{task}.csv".format(directoryPath=directoryPath, project_name=project, task = task)
-
-    correlationsFiles = open(outputFile.format(project=project), "w")
-    
     
     # BEGIN Core Contributors
     df = pd.read_csv(tasks)
@@ -40,16 +41,14 @@ for project in ["angular"]:
     corr_module_t_line, _ = pearsonr(df[c.MODULE_CC], df[c.T_LINE])
 
 
-    print('{0}:{1} - Pearsons correlation:'.format(project, task))
-    print('E_Module => NT_CC: {0}'.format(corr_module_nt_cc))
-    print('E_Module => NO_CC: {0}'.format(corr_module_no_cc))
-    print('E_Module => T_CC: {0}'.format(corr_module_t_cc))
-    print('E_Module => NT_EC: {0}'.format(corr_module_nt_ec))
-    print('E_Module => NO_CC: {0}'.format(corr_module_no_ec))
-    print('E_Module => T_EC: {0}'.format(corr_module_t_ec))
-    print('E_Module => T_Line: {0}\n'.format(corr_module_t_line))
-
-    correlationsFiles.write('E_Module => NT_CC: {0}'.format(corr_module_nt_cc))
+    correlationsFile.write('{0}:{1} - Pearsons correlation:\n'.format(project, task))
+    correlationsFile.write('E_Module => NT_CC: {0}\n'.format(corr_module_nt_cc))
+    correlationsFile.write('E_Module => NO_CC: {0}\n'.format(corr_module_no_cc))
+    correlationsFile.write('E_Module => T_CC: {0}\n'.format(corr_module_t_cc))
+    correlationsFile.write('E_Module => NT_EC: {0}\n'.format(corr_module_nt_ec))
+    correlationsFile.write('E_Module => NO_CC: {0}\n'.format(corr_module_no_ec))
+    correlationsFile.write('E_Module => T_EC: {0}\n'.format(corr_module_t_ec))
+    correlationsFile.write('E_Module => T_Line: {0}\n\n'.format(corr_module_t_line))
 
     corr_line_nt_cc, _ = pearsonr(df[c.LINE_CC], df[c.NT_CC])
     corr_line_no_cc, _ = pearsonr(df[c.LINE_CC], df[c.NO_CC])
@@ -59,15 +58,14 @@ for project in ["angular"]:
     corr_line_t_ec, _ = pearsonr(df[c.LINE_CC], df[c.T_CC])
     corr_line_t_line, _ = pearsonr(df[c.LINE_CC], df[c.T_LINE])
 
-
-    print('{0}:{1} - Pearsons correlation:'.format(project, task))
-    print('E_Line => NT_CC: {0}'.format(corr_line_nt_cc))
-    print('E_Line => NO_CC: {0}'.format(corr_line_no_cc))
-    print('E_Line => T_CC: {0}'.format(corr_line_t_cc))
-    print('E_Line => NT_EC: {0}'.format(corr_line_nt_ec))
-    print('E_Line => NO_CC: {0}'.format(corr_line_no_ec))
-    print('E_Line => T_EC: {0}'.format(corr_line_t_ec))
-    print('E_Line => T_Line: {0}\n'.format(corr_line_t_line))
+    correlationsFile.write('{0}:{1} - Pearsons correlation:\n'.format(project, task))
+    correlationsFile.write('E_Line => NT_CC: {0}\n'.format(corr_line_nt_cc))
+    correlationsFile.write('E_Line => NO_CC: {0}\n'.format(corr_line_no_cc))
+    correlationsFile.write('E_Line => T_CC: {0}\n'.format(corr_line_t_cc))
+    correlationsFile.write('E_Line => NT_EC: {0}\n'.format(corr_line_nt_ec))
+    correlationsFile.write('E_Line => NO_CC: {0}\n'.format(corr_line_no_ec))
+    correlationsFile.write('E_Line => T_EC: {0}\n'.format(corr_line_t_ec))
+    correlationsFile.write('E_Line => T_Line: {0}\n\n'.format(corr_line_t_line))
 
     corr_module_nt_cc, _ = spearmanr(df[c.MODULE_CC], df[c.NT_CC])
     corr_module_no_cc, _ = spearmanr(df[c.MODULE_CC], df[c.NO_CC])
@@ -78,14 +76,14 @@ for project in ["angular"]:
     corr_module_t_line, _ = spearmanr(df[c.MODULE_CC], df[c.T_LINE])
 
 
-    print('{0}:{1} - Spearman correlation:'.format(project, task))
-    print('E_Module => NT_CC: {0}'.format(corr_module_nt_cc))
-    print('E_Module => NO_CC: {0}'.format(corr_module_no_cc))
-    print('E_Module => T_CC: {0}'.format(corr_module_t_cc))
-    print('E_Module => NT_EC: {0}'.format(corr_module_nt_ec))
-    print('E_Module => NO_CC: {0}'.format(corr_module_no_ec))
-    print('E_Module => T_EC: {0}'.format(corr_module_t_ec))
-    print('E_Module => T_Line: {0}\n'.format(corr_module_t_line))
+    correlationsFile.write('{0}:{1} - Spearman correlation:\n'.format(project, task))
+    correlationsFile.write('E_Module => NT_CC: {0}\n'.format(corr_module_nt_cc))
+    correlationsFile.write('E_Module => NO_CC: {0}\n'.format(corr_module_no_cc))
+    correlationsFile.write('E_Module => T_CC: {0}\n'.format(corr_module_t_cc))
+    correlationsFile.write('E_Module => NT_EC: {0}\n'.format(corr_module_nt_ec))
+    correlationsFile.write('E_Module => NO_CC: {0}\n'.format(corr_module_no_ec))
+    correlationsFile.write('E_Module => T_EC: {0}\n'.format(corr_module_t_ec))
+    correlationsFile.write('E_Module => T_Line: {0}\n\n'.format(corr_module_t_line))
 
     corr_line_nt_cc, _ = spearmanr(df[c.LINE_CC], df[c.NT_CC])
     corr_line_no_cc, _ = spearmanr(df[c.LINE_CC], df[c.NO_CC])
@@ -95,16 +93,16 @@ for project in ["angular"]:
     corr_line_t_ec, _ = spearmanr(df[c.LINE_CC], df[c.T_CC])
     corr_line_t_line, _ = spearmanr(df[c.LINE_CC], df[c.T_LINE])
 
-    print('{0}:{1} - Spearman correlation:'.format(project, task))
-    print('E_Line => NT_CC: {0}'.format(corr_line_nt_cc))
-    print('E_Line => NO_CC: {0}'.format(corr_line_no_cc))
-    print('E_Line => T_CC: {0}'.format(corr_line_t_cc))
-    print('E_Line => NT_EC: {0}'.format(corr_line_nt_ec))
-    print('E_Line => NO_CC: {0}'.format(corr_line_no_ec))
-    print('E_Line => T_EC: {0}'.format(corr_line_t_ec))
-    print('E_Line => T_Line: {0}\n'.format(corr_line_t_line))
+    correlationsFile.write('{0}:{1} - Spearman correlation:\n'.format(project, task))
+    correlationsFile.write('E_Line => NT_CC: {0}\n'.format(corr_line_nt_cc))
+    correlationsFile.write('E_Line => NO_CC: {0}\n'.format(corr_line_no_cc))
+    correlationsFile.write('E_Line => T_CC: {0}\n'.format(corr_line_t_cc))
+    correlationsFile.write('E_Line => NT_EC: {0}\n'.format(corr_line_nt_ec))
+    correlationsFile.write('E_Line => NO_CC: {0}\n'.format(corr_line_no_ec))
+    correlationsFile.write('E_Line => T_EC: {0}\n'.format(corr_line_t_ec))
+    correlationsFile.write('E_Line => T_Line: {0}\n\n'.format(corr_line_t_line))
 
-    correlationsFiles.close()
+  correlationsFile.close()
 
     
 
